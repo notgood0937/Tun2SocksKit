8a60001beb9576faa68da57f760e8082e152810f
notgood0937 <notgoood0931@gmail.com>
2024-11-26 10:55:23 +0800

    first commit

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version: 5.7
+
+import PackageDescription
+
+let package = Package(
+  name: "Tun2SocksKit",
+  platforms: [.iOS(.v15), .macOS(.v10_14), .tvOS(.v17)],
+  products: [
+    .library(
+        name: "Tun2SocksKit",
+        targets: ["Tun2SocksKit"]
+    ),
+    .library(
+        name: "Tun2SocksKitC",
+        targets: ["Tun2SocksKitC"]
+    )
+  ],
+  targets: [
+    .target(
+        name: "Tun2SocksKit",
+        dependencies: ["HevSocks5Tunnel", "Tun2SocksKitC"]
+    ),
+    .target(
+        name: "Tun2SocksKitC",
+        publicHeadersPath: "."
+    ),
+    .binaryTarget(
+        name: "HevSocks5Tunnel",
+        url: "https://github.com/EbrahimTahernejad/Tun2SocksKit/releases/download/3.7.4/HevSocks5Tunnel.xcframework.zip",
+        checksum: "43749279fef5be8576099257352edf6b2832cc82fc3b922af1b63e7c9c918bc9"
+    )
+  ]
+)
